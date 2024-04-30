5317a3e61d55b1fc301a75325d3fd8eefabffcf3
Ashmita Bhattacharjee <150361021+bhattacharjee-ashmita@users.noreply.github.com>
2024-04-30 22:49:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,134 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import Image,ImageTk
+from student import Student
+
+class Face_Recognition_System:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("Face Recognition System")
+
+        img=Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\Banner1.jpg")
+        img=img.resize((1530,150),Image.ANTIALIAS)
+        self.photoimg=ImageTk.PhotoImage(img)
+
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=1530,height=150)
+
+        #bg image
+        img1=Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\BG.jpg")
+        img1=img1.resize((1530,640),Image.ANTIALIAS)
+        self.photoimg1=ImageTk.PhotoImage(img1)
+
+        bg_img=Label(self.root,image=self.photoimg1)
+        bg_img.place(x=0,y=150,width=1530,height=640)
+
+        title_lbl=Label(bg_img,text="FACE RECOGNITION ATTENDANCE SYSTEM",font=("times new roman",30,"bold"),bg="navy blue",fg="white")
+        title_lbl.place(x=0,y=0,width=1530,height=45)
+
+
+        #student button
+        img4 = Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\b1.jpg")
+        img4 = img4.resize((180,180),Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b1=Button(bg_img,image=self.photoimg4,command=self.student_details,cursor="hand2")
+        b1.place(x=200,y=100,width=180,height=180)
+
+        b1_1=Button(bg_img,text="Student Details",command=self.student_details,cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200,y=260,width=180,height=30)
+
+        #Detect Face button
+        img5 = Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\b2.jpg")
+        img5 = img5.resize((180,180),Image.ANTIALIAS)
+        self.photoimg5=ImageTk.PhotoImage(img5)
+
+        b1=Button(bg_img,image=self.photoimg5,cursor="hand2")
+        b1.place(x=500,y=100,width=180,height=180)
+
+        b1_1=Button(bg_img,text="Face Detector",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=500,y=260,width=180,height=30)
+
+        #Attendance button
+        img6 = Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\b3.jpg")
+        img6 = img6.resize((180,180),Image.ANTIALIAS)
+        self.photoimg6=ImageTk.PhotoImage(img6)
+
+        b1=Button(bg_img,image=self.photoimg6,cursor="hand2")
+        b1.place(x=800,y=100,width=180,height=180)
+
+        b1_1=Button(bg_img,text="Attendance",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=800,y=260,width=180,height=30)
+
+        #Help button
+        img7 = Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\b4.jpg")
+        img7 = img7.resize((180,180),Image.ANTIALIAS)
+        self.photoimg7=ImageTk.PhotoImage(img7)
+
+        b1=Button(bg_img,image=self.photoimg7,cursor="hand2")
+        b1.place(x=1100,y=100,width=180,height=180)
+
+        b1_1=Button(bg_img,text="Help Desk",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=1100,y=260,width=180,height=30)
+
+
+        #Train button
+        img8 = Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\b5.jpg")
+        img8 = img8.resize((180,180),Image.ANTIALIAS)
+        self.photoimg8=ImageTk.PhotoImage(img8)
+
+        b1=Button(bg_img,image=self.photoimg8,cursor="hand2")
+        b1.place(x=200,y=350,width=180,height=180)
+
+        b1_1=Button(bg_img,text="Train Data",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200,y=520,width=180,height=30)
+
+        #Photos button
+        img9 = Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\b6.jpg")
+        img9 = img9.resize((180,180),Image.ANTIALIAS)
+        self.photoimg9=ImageTk.PhotoImage(img9)
+
+        b1=Button(bg_img,image=self.photoimg9,cursor="hand2")
+        b1.place(x=500,y=350,width=180,height=180)
+
+        b1_1=Button(bg_img,text="Photos",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=500,y=520,width=180,height=30)
+
+        #Developer button
+        img10 = Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\b7.jpg")
+        img10 = img10.resize((180,180),Image.ANTIALIAS)
+        self.photoimg10=ImageTk.PhotoImage(img10)
+
+        b1=Button(bg_img,image=self.photoimg10,cursor="hand2")
+        b1.place(x=800,y=350,width=180,height=180)
+
+        b1_1=Button(bg_img,text="Developer",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=800,y=520,width=180,height=30)
+
+        #Exit button
+        img11 = Image.open(r"C:\Users\Ashmita\Desktop\Face Recognition System\collage_images\b8.jpg")
+        img11 = img11.resize((180,180),Image.ANTIALIAS)
+        self.photoimg11=ImageTk.PhotoImage(img11)
+
+        b1=Button(bg_img,image=self.photoimg11,cursor="hand2")
+        b1.place(x=1100,y=350,width=180,height=180)
+
+        b1_1=Button(bg_img,text="Exit",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=1100,y=520,width=180,height=30)
+
+# ==================Functions Buttons=======================
+
+    def student_details(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Student(self.new_window)
+        
+
+
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=Face_Recognition_System(root)
+    root.mainloop()
+
+
